d17ebdd575c223c75284f209e04da1d1750932c5
hbabashah <hossein.babashah3@gmail.com>
2021-07-01 15:49:36 +0200

    =ni acq time out fixed

--- hardware/ni_card_acquisition.py
+++ hardware/ni_card_acquisition.py
@@ -20,11 +20,11 @@ class NICard_Acquisition(Base, dummy_interface):
     """
 
     # config options
-    _device_name = ConfigOption(name='device_name', default='Dev1', missing='warn')
-    _device_name_ao = ConfigOption(name='device_name_ao', default='Dev3', missing='warn')
+    _device_name_slow = ConfigOption(name='device_name_slow', default='Dev1', missing='warn')
+    _device_name_fast = ConfigOption(name='device_name_fast', default='Dev3', missing='warn')
     _analog_inputs = ConfigOption(name='analog_inputs', default=list(), missing='warn')
     _analog_outputs = ConfigOption(name='analog_outputs', default=list(), missing='warn')
-    _trigger_terminal = ConfigOption(name='trigger_terminal', default='PFI0', missing='info')
+    _trigger_terminal = ConfigOption(name='trigger_terminal', default='PFI8', missing='info')
     _sampling_frequency = ConfigOption(name='sampling_frequency', default=200*10**3, missing='info')
 
 
@@ -44,12 +44,12 @@ class NICard_Acquisition(Base, dummy_interface):
         # Create the list of channels to acquire
         self.nb_chan = 0
         for channel in self._analog_inputs:
-            daq.CreateAIVoltageChan(self._ai_task, self._device_name+'/'+channel, None, daq.DAQmx_Val_RSE, -10, 10, daq.DAQmx_Val_Volts, None) # RSE or NRSE or Diff
+            daq.CreateAIVoltageChan(self._ai_task, self._device_name_fast+'/'+channel, None, daq.DAQmx_Val_RSE, -10, 10, daq.DAQmx_Val_Volts, None) # RSE or NRSE or Diff
             self.nb_chan += 1
 
         # self.nb_chan = 0
         # for channel in self._analog_outputs:
-        #     daq.CreateAOVoltageChan(self._ao_task, self._device_name_ao+'/'+channel, None, daq.DAQmx_Val_Diff, -10, 10, daq.DAQmx_Val_Volts, None) # RSE or NRSE or Diff
+        #     daq.CreateAOVoltageChan(self._ao_task, self._device_name_slow+'/'+channel, None, daq.DAQmx_Val_Diff, -10, 10, daq.DAQmx_Val_Volts, None) # RSE or NRSE or Diff
         #     self.nb_chan += 1
 
 
@@ -58,7 +58,9 @@ class NICard_Acquisition(Base, dummy_interface):
         daq.DAQmxClearTask(self._ai_task)
 
     def set_timing(self, acquisition_time):
-        """ H.Babashah - Define the timing of the acquisition. """
+        """ 
+		H.Babashah - Define the timing of the acquisition. 
+		"""
         self._acquisition_time = acquisition_time
         self.nb_samps_per_chan = int(self._acquisition_time*self._sampling_frequency)
         self._buffer_size = int(self._acquisition_time*self._sampling_frequency*self.nb_chan)
@@ -71,8 +73,15 @@ class NICard_Acquisition(Base, dummy_interface):
         #                      daq.DAQmx_Val_FiniteSamps, self.numSamples)
 
         self.data = np.ndarray((self.nb_chan+1, self.nb_samps_per_chan))
+    def get_timing(self):
+	    """ 
+		H.Babashah - return the sampling frequency of DAQ 
+		"""
+        return self._sampling_frequency
     def set_trigger(self, edge):
-        """ H.Babashah - Define the edge of the external trigger. """
+        """ 
+		H.Babashah - Define the edge of the external trigger. 
+		"""
 
         if edge == 'Rising':
             daq.CfgDigEdgeStartTrig(self._ai_task, self._trigger_terminal, daq.DAQmx_Val_Rising)
@@ -96,36 +105,40 @@ class NICard_Acquisition(Base, dummy_interface):
 
 
     def set_refrence_trigger(self, edge,pretriggerSamples):
-        """ H.Babashah - It acquire the data before the trigger """
+        """ 
+		H.Babashah - It acquire the data before the trigger 
+		"""
         if edge == 'Rising':
             daq.CfgDigEdgeRefTrig(self._ai_task,self._trigger_terminal, daq.DAQmx_Val_Rising, pretriggerSamples)  # C channel DDG
         elif edge == 'Falling':
             daq.CfgDigEdgeRefTrig(self._ai_task,self._trigger_terminal, daq.DAQmx_Val_Falling, pretriggerSamples)  # C channel DDG
 
     def start_acquisition(self):
-        """ H.Babashah - Start the acquisition task.
+        """ 
+		H.Babashah - Start the acquisition task.
+		
         It seems that using the start task generates an error when trying to read data several times for "continuous acquisition".
-        Only calling read several times seems fine. """
+        Only calling read several times seems fine. 
+		"""
         pass #daq.DAQmxStartTask(self._ai_task)
 
     def stop_acquisition(self):
+		""" 
+		H.Babashah - stop aquiring  the data from ni card task. 
+		"""
         daq.DAQmxStopTask(self._ai_task)
 
     def read_data(self):
+		"""
+		H.Babashah - read the data from ni card task. 
+		"""
+	
         time_data = np.linspace(0, self._acquisition_time, int(self.nb_samps_per_chan))
         t0 = time.time()
-        daq.ReadAnalogF64(self._ai_task, self.nb_samps_per_chan, 50, daq.DAQmx_Val_GroupByChannel, self.raw_data, self._buffer_size, ctypes.byref(self.read), None)
+        daq.ReadAnalogF64(self._ai_task, self.nb_samps_per_chan, 150, daq.DAQmx_Val_GroupByChannel, self.raw_data, self._buffer_size, ctypes.byref(self.read), None)
         t1 = time.time()
         print(t1-t0)
         self.data[0] = time_data
         for i in range(self.nb_chan):
             self.data[i+1] = np.split(self.raw_data, self.nb_chan)[i]
         return self.data
-
-    # def write_ao(self,ao_value):
-    #     self._ao_value=ao_value
-    #     time_data = np.linspace(0, self._acquisition_time, int(self.nb_samps_per_chan))
-    #     daq.ReadAnalogF64(self._ai_task, self.nb_samps_per_chan, 50, daq.DAQmx_Val_GroupByChannel, self.raw_data, self._buffer_size, ctypes.byref(self.read), None)
-    #     daq.WriteAnalogF64(self._ao_task,numSampsPerChan=self.numSamples, autoStart=True, timeout=1.0, dataLayout=daq.DAQmx_Val_GroupByChannel, writeArray=self._ao_value, reserved=None,
-    #                                  sampsPerChanWritten=ctypes.byref(self.samplesWritten))
-
