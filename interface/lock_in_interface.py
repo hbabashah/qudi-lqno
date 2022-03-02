8025d909bcf818a0bfc59384618deec21f2dbc57
hbabashah <hossein.babashah3@gmail.com>
2022-03-02 14:59:01 +0100

    interface for lockin hardwares

---
+++ interface/lock_in_interface.py
@@ -0,0 +1,9 @@
+from core.interface import abstract_interface_method
+from core.meta import InterfaceMetaclass
+
+
+class LockinInterface(metaclass=InterfaceMetaclass):
+    """
+    This is the Interface class for Lock-in detection.
+    """
+    pass
